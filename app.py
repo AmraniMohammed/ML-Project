aef2e74f15be5bc9c3b311b69ba13b693a8580da
amrani alaoui mohammed <mrani.med98@gmail.com>
2022-05-20 16:43:38 +0100

    adding exlporation

--- app.py
+++ app.py
@@ -55,6 +55,15 @@ from bokeh.palettes import Set3
 import time 
 
 
+##########################################
+import numpy as np
+import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+##########################################
+
 
 # Main Predicor class
 class Predictor:
@@ -312,7 +321,7 @@ class Predictor:
 
 selected = option_menu(
                 menu_title=None,  # required
-                options=["Description", "AutoML", "SemiAutoML"],  # required
+                options=["Exploration", "AutoML", "SemiAutoML"],  # required
                 icons=["book", "command", "person"],  # optional
                 menu_icon="cast",  # optional
                 default_index=0,  # optional
@@ -334,6 +343,43 @@ if selected == "Description":
     #### SemiAutoML
     SemiAutoML is a machine learning tool that semi automates the process of selecting the best model for a given dataset by a **user friendly interface**.
     """)
+
+if selected == "Exploration":
+    st.title(f"You have selected {selected}")
+    with st.sidebar.header('1. Upload your CSV data'):
+        uploaded_file_exploration = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+
+    # Pandas Profiling Report
+    if uploaded_file_exploration is not None:
+        @st.cache
+        def load_csv():
+            csv = pd.read_csv(uploaded_file_exploration)
+            return csv
+        df = load_csv()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
+    else:
+        st.info('Awaiting for CSV file to be uploaded.')
+        if st.button('Press to use Example Dataset'):
+            # Example data
+            @st.cache
+            def load_data():
+                a = pd.DataFrame(
+                    np.random.rand(100, 5),
+                    columns=['a', 'b', 'c', 'd', 'e']
+                )
+                return a
+            df = load_data()
+            pr = ProfileReport(df, explorative=True)
+            st.header('**Input DataFrame**')
+            st.write(df)
+            st.write('---')
+            st.header('**Pandas Profiling Report**')
+            st_profile_report(pr)
 if selected == "AutoML":
     st.title(f"You have selected {selected}")
     #---------------------------------#
